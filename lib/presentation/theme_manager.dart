ae1a16976583806a8ff244d8f4b64c461a693e79
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-06-20 03:29:19 +0200

    app bar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/color_manager.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/font_manager.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/styles_manager.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -8,10 +11,24 @@ ThemeData getApplicationTheme() {
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary, // ripple effect color
     // cardview theme
-
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
     // appbar theme
-
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle: getRegularStyle(
+        fontSize: FontSize.s16,
+        color: ColorManager.white,
+      ),
+    ),
     // button theme
 
     // text theme
