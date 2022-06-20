a1321fe62cc21ab4f5b89db2f01ed4542523fb58
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-06-20 03:40:25 +0200

    button theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -30,6 +30,28 @@ ThemeData getApplicationTheme() {
       ),
     ),
     // button theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.lightPrimary,
+    ),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s17,
+        ),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            AppSize.s12,
+          ),
+        ),
+      ),
+    ),
 
     // text theme
 
