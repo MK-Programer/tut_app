e4f035841a96951ebc37ee1571c4925209163f34
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-06-21 04:26:56 +0200

    onboarding screen

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/resources/assets_manager.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/resources/color_manager.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/resources/constants_manager.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/resources/routes_manager.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/resources/string_manager.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/resources/values_manager.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -20,14 +22,14 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   int _currentIndex = 0;
 
   List<SliderObject> _getSliderData() => [
-        SliderObject(AppStrings.noBoardingTitle1,
-            AppStrings.noBoardingSubTitle1, ImageAssets.onBoardingLogo1),
-        SliderObject(AppStrings.noBoardingTitle2,
-            AppStrings.noBoardingSubTitle2, ImageAssets.onBoardingLogo2),
-        SliderObject(AppStrings.noBoardingTitle3,
-            AppStrings.noBoardingSubTitle3, ImageAssets.onBoardingLogo3),
-        SliderObject(AppStrings.noBoardingTitle4,
-            AppStrings.noBoardingSubTitle4, ImageAssets.onBoardingLogo1),
+        SliderObject(AppStrings.onBoardingTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onBoardingLogo1),
+        SliderObject(AppStrings.onBoardingTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onBoardingLogo2),
+        SliderObject(AppStrings.onBoardingTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onBoardingLogo3),
+        SliderObject(AppStrings.onBoardingTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onBoardingLogo4),
       ];
 
   @override
@@ -35,41 +37,44 @@ class _OnBoardingViewState extends State<OnBoardingView> {
     return Scaffold(
       backgroundColor: ColorManager.white,
       appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        elevation: AppSize.s0,
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarColor: ColorManager.white,
           statusBarBrightness: Brightness.dark,
         ),
       ),
       body: PageView.builder(
-        controller: _pageController,
-        itemCount: _list.length,
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        itemBuilder: (context, index) {
-          // return onboarding page
-          return OnBoardingPage(_list[index]);
-        },
-      ),
+          controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+            return OnBoardingPage(_list[index]);
+          }),
       bottomSheet: Container(
         color: ColorManager.white,
-        height: AppSize.s100,
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Align(
               alignment: Alignment.centerRight,
               child: TextButton(
-                onPressed: () {},
-                child: const Text(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.loginRoute);
+                },
+                child: Text(
                   AppStrings.skip,
+                  style: Theme.of(context).textTheme.titleMedium,
                   textAlign: TextAlign.end,
                 ),
               ),
             ),
 
-            // widget indicator and arrows
+            // widgets indicator and arrows
             _getBottomSheetWidget()
           ],
         ),
@@ -78,55 +83,94 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   }
 
   Widget _getBottomSheetWidget() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        // left arrow
-        Padding(
-          padding: const EdgeInsets.all(
-            AppPadding.p14,
-          ),
-          child: GestureDetector(
-            child: SizedBox(
-              width: AppSize.s20,
-              height: AppSize.s20,
-              child: SvgPicture.asset(
-                ImageAssets.leftArrowIc,
-              ),
+    return Container(
+      color: ColorManager.primary,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // left arrow
+          Padding(
+            padding: const EdgeInsets.all(
+              AppPadding.p14,
             ),
-          ),
-        ),
-
-        // circle indicator
-        Row(
-          children: [
-            for (int i = 0; i < _list.length; i++)
-              Padding(
-                padding: const EdgeInsets.all(
-                  AppPadding.p8,
+            child: GestureDetector(
+              child: SizedBox(
+                width: AppSize.s20,
+                height: AppSize.s20,
+                child: SvgPicture.asset(
+                  ImageAssets.leftArrowIc,
                 ),
-                child: _getProperCircle(i),
               ),
-          ],
-        ),
-
-        // right arrow
-        Padding(
-          padding: const EdgeInsets.all(
-            AppPadding.p14,
-          ),
-          child: GestureDetector(
-            child: SizedBox(
-              width: AppSize.s20,
-              height: AppSize.s20,
-              child: SvgPicture.asset(
-                ImageAssets.rightArrowIc,
-              ),
+              onTap: () {
+                // go to previous slide
+                _pageController.animateToPage(
+                  _getPreviousIndex(),
+                  duration: const Duration(
+                    milliseconds: AppConstants.sliderAnimationTime,
+                  ),
+                  curve: Curves.bounceInOut,
+                );
+              },
             ),
           ),
-        ),
-      ],
+
+          // circle indicator
+
+          // right arrow
+
+          Row(
+            children: [
+              for (int i = 0; i < _list.length; i++)
+                Padding(
+                  padding: const EdgeInsets.all(
+                    AppPadding.p8,
+                  ),
+                  child: _getProperCircle(i),
+                )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(
+              AppPadding.p14,
+            ),
+            child: GestureDetector(
+                child: SizedBox(
+                  width: AppSize.s20,
+                  height: AppSize.s20,
+                  child: SvgPicture.asset(
+                    ImageAssets.rightArrowIc,
+                  ),
+                ),
+                onTap: () {
+                  // go to previous slide
+                  _pageController.animateToPage(
+                    _getNextIndex(),
+                    duration: const Duration(
+                      milliseconds: AppConstants.sliderAnimationTime,
+                    ),
+                    curve: Curves.bounceInOut,
+                  );
+                }),
+          )
+        ],
+      ),
     );
+  }
+
+  int _getPreviousIndex() {
+    int previousIndex = --_currentIndex;
+    if (previousIndex == -1) {
+      previousIndex = _list.length - 1;
+    }
+    return previousIndex;
+  }
+
+  int _getNextIndex() {
+    int nextIndex = ++_currentIndex;
+    if (nextIndex == _list.length) {
+      nextIndex = 0;
+    }
+    return nextIndex;
   }
 
   Widget _getProperCircle(int index) {
--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,17 +1,17 @@
 class AppStrings {
   static const noRouteFound = 'No Route Found';
-  static const noBoardingTitle1 = 'SEE THE BEST COURSE #1';
-  static const noBoardingTitle2 = 'SEE THE BEST COURSE #2';
-  static const noBoardingTitle3 = 'SEE THE BEST COURSE #3';
-  static const noBoardingTitle4 = 'SEE THE BEST COURSE #4';
+  static const onBoardingTitle1 = 'SEE THE BEST COURSE #1';
+  static const onBoardingTitle2 = 'SEE THE BEST COURSE #2';
+  static const onBoardingTitle3 = 'SEE THE BEST COURSE #3';
+  static const onBoardingTitle4 = 'SEE THE BEST COURSE #4';
 
-  static const noBoardingSubTitle1 =
+  static const onBoardingSubTitle1 =
       'Tut app is an awesome flutter application using clean architecture #1';
-  static const noBoardingSubTitle2 =
+  static const onBoardingSubTitle2 =
       'Tut app is an awesome flutter application using clean architecture #2';
-  static const noBoardingSubTitle3 =
+  static const onBoardingSubTitle3 =
       'Tut app is an awesome flutter application using clean architecture #3';
-  static const noBoardingSubTitle4 =
+  static const onBoardingSubTitle4 =
       'Tut app is an awesome flutter application using clean architecture #4';
 
   static const skip = 'Skip';
