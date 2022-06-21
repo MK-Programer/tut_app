0078fce8f29301951a48466d324b0a3dddf227df
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-06-21 22:29:13 +0200

    stream controller to the onboarding screen

--- lib/presentation/base/baseviewmodel.dart
+++ lib/presentation/base/baseviewmodel.dart
@@ -1,4 +1,6 @@
 // abstract because it will be used to inherit from it
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/models.dart';
+
 abstract class BaseViewModel extends BaseViewModelInputs
     with BaseViewModelOutputs {
   // shared variables and functions that will be used through any viewmodel
--- lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/models.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/base/baseviewmodel.dart';
 
 class OnBoardingViewModel extends BaseViewModel
     with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
   // stream controllers outputs
+  StreamController _streamController = StreamController<SliderViewObject>();
 
   // OnBoarding ViewModelInputs
   @override
@@ -19,6 +23,13 @@ class OnBoardingViewModel extends BaseViewModel
 
   @override
   void onPageChanged(int index) {}
+
+  @override
+  Sink get inputSliderViewObject => throw UnimplementedError();
+
+  // onboarding view model outputs
+  @override
+  Stream get outputSliderViewObject => throw UnimplementedError();
 }
 
 // inputs mean that "Orders" that our view model will recieve from  view
@@ -26,6 +37,11 @@ abstract class OnBoardingViewModelInputs {
   void goNext(); // when user clicks on right arrow or swip left
   void goPrevious(); // when user clicks on left arrow or swip right
   void onPageChanged(int index);
+  // stream controller input
+  Sink get inputSliderViewObject;
 }
 
-abstract class OnBoardingViewModelOutputs {}
+abstract class OnBoardingViewModelOutputs {
+  // stream controller output
+  Stream get outputSliderViewObject;
+}
