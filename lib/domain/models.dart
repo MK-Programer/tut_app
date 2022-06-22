3469a44c9771e2cfa8445d23acfeb5ea8abe5d50
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-06-22 23:46:41 +0200

    update models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -14,3 +14,27 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.numberOfSlides, this.currentIndex);
 }
+
+// login models
+class Customer {
+  String id;
+  String name;
+  String numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
