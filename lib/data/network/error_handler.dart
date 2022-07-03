f412a93d391da9f80406d15b3132ed6391bbf842
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-03 15:39:20 +0200

    data layer - adding responses messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -32,3 +32,28 @@ class ResponseCode {
   static const int NO_INTERNET_CONNECTION = -6;
   static const int UNKOWN = -7;
 }
+
+class ResponseMessage {
+  static const String SUCCESS = "Success"; // success with data
+  static const String NO_CONTENT =
+      "No content"; // success with no data (no content)
+  static const String BAD_REQUEST =
+      "Bad request, Try again later"; // failure, API rejected request
+  static const String FORBIDDEN =
+      "Forbidden request, Try again later"; // failure, API rejected request
+  static const String UNAUTHORIZED =
+      "User is unauthorized , Try again later"; // failure, user is not authorized
+  // static const int NOT_FOUND = 200;
+  static const String INTERNAL_SERVER_ERROR =
+      "Something went wrong, Try again later"; // failure, crash in server side
+
+  // local status code
+  static const String CONNECT_TIMEOUT = "Timeout error, Try again later";
+  static const String CANCEL = "Request was cancelled, Try again later";
+  static const String RECIEVE_TIMEOUT = "Timeout error, Try again later";
+  static const String SEND_TIMEOUT = "Timeout error, Try again later";
+  static const String CACHE_ERROR = "Cache error, Try again later";
+  static const String NO_INTERNET_CONNECTION =
+      "Please check your internet connection";
+  static const String UNKOWN = "Something went wrong, Try again later";
+}
