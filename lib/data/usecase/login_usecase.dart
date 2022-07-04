a04fc7ea87a8de25a1c205b99a6cf7fb8b536ebc
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-04 12:27:00 +0200

    domain layer- adding  login usecase

---
+++ lib/data/usecase/login_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_advanced_clean_architecture_with_mvvm/data/network/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/data/network/requests.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/data/usecase/base_usecase.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/model/models.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/repository/repository.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  final Repository _repository;
+
+  LoginUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      LoginUseCaseInput input) async {
+    return await _repository.login(LoginRequest(input.email, input.password));
+  }
+}
+
+class LoginUseCaseInput {
+  String email;
+  String password;
+
+  LoginUseCaseInput(this.email, this.password);
+}
