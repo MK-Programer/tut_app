bf7684e8989efaba7f72cf13d62a3562979d4789
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-04 12:02:59 +0200

    application layer - adding application preferences

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -7,7 +7,7 @@ const String APPLICATION_JSON = "application/json";
 const String CONTENT_TYPE = "content-type";
 const String ACCEPT = "accept";
 const String AUTHORIZATION = "authorization";
-const String DEFAULT_LANGUAGE = "language";
+const String DEFAULT_LANGUAGE = "en";
 
 class DioFactory {
   Future<Dio> getDio() async {
