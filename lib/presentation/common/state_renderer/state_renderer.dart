cceb6cbb2a57a8f265e4ae733c00825485a470c5
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-05 18:02:42 +0200

    adding state renderer implementation part 2

--- lib/presentation/common/state_renderer/state_renderer.dart
+++ lib/presentation/common/state_renderer/state_renderer.dart
@@ -35,4 +35,28 @@ class StateRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container();
   }
+
+  Widget _getStateWidget() {
+    switch (stateRendererType) {
+      case StateRendererType.popupLoadingState:
+
+      case StateRendererType.popupErrorState:
+
+      case StateRendererType.fullScreenLoadingState:
+
+      case StateRendererType.fullScreenErrorState:
+
+      case StateRendererType.fullScreenEmptyState:
+
+      case StateRendererType.contentState:
+    }
+  }
+
+  Widget _getItemsColumn(List<Widget> children) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: children,
+    );
+  }
 }
