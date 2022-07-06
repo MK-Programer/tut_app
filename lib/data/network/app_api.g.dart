f3d9b6dc60c26011f323e713b57ac2f8a2af3766
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-06 15:19:10 +0200

    update the api url in app_api.g.dart

--- lib/data/network/app_api.g.dart
+++ lib/data/network/app_api.g.dart
@@ -55,7 +55,7 @@ class _AppSerivceClient implements AppSerivceClient {
     final _result = await _dio.fetch<Map<String, dynamic>>(
         _setStreamType<ForgetPasswordResponse>(
             Options(method: 'POST', headers: _headers, extra: _extra)
-                .compose(_dio.options, '/customers/forgotPassword',
+                .compose(_dio.options, '/customers/forgetPassword',
                     queryParameters: queryParameters, data: _data)
                 .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
     final value = ForgetPasswordResponse.fromJson(_result.data!);
