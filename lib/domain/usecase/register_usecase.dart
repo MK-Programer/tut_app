8b3152bbad546132435aa8a730cfcc59b1302648
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-19 15:29:46 +0200

    add register api call in repository impl, remote data source and usecase

---
+++ lib/domain/usecase/register_usecase.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_advanced_clean_architecture_with_mvvm/data/network/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/data/network/requests.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/model/models.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/repository/repository.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/usecase/base_usecase.dart';
+
+class RegisterUseCase
+    implements BaseUseCase<RegisterUseCaseInput, Authentication> {
+  final Repository _repository;
+
+  RegisterUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      RegisterUseCaseInput input) async {
+    return await _repository.register(
+      RegisterRequest(
+        input.userName,
+        input.countryMobileCode,
+        input.mobileNumber,
+        input.email,
+        input.password,
+        input.profilePicture,
+      ),
+    );
+  }
+}
+
+class RegisterUseCaseInput {
+  String userName;
+  String countryMobileCode;
+  String mobileNumber;
+  String email;
+  String password;
+  String profilePicture;
+
+  RegisterUseCaseInput(
+    this.userName,
+    this.countryMobileCode,
+    this.mobileNumber,
+    this.email,
+    this.password,
+    this.profilePicture,
+  );
+}
