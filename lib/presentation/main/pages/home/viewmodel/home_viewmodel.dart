8e1489f5711ca3c167c62bd9c95006140472200a
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-07-22 16:40:08 +0200

    adding implementation of home api  in view model

--- lib/presentation/main/pages/home/viewmodel/home_viewmodel.dart
+++ lib/presentation/main/pages/home/viewmodel/home_viewmodel.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/model/models.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/domain/usecase/home_usecase.dart';
 import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/base/baseviewmodel.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/common/state_renderer/state_renderer.dart';
+import 'package:flutter_advanced_clean_architecture_with_mvvm/presentation/common/state_renderer/state_renderer_impl.dart';
 import 'package:rxdart/rxdart.dart';
 
 class HomeViewModel extends BaseViewModel
@@ -18,7 +21,38 @@ class HomeViewModel extends BaseViewModel
   HomeViewModel(this._homeUseCase);
 
   @override
-  void start() {}
+  void start() {
+    _getHomeData();
+  }
+
+  _getHomeData() async {
+    inputState.add(
+      LoadingState(
+        stateRendererType: StateRendererType.fullScreenLoadingState,
+      ),
+    );
+    (await _homeUseCase.execute(Void)).fold(
+      (failure) => {
+        // left -> failure
+        inputState.add(
+          ErrorState(
+            StateRendererType.fullScreenErrorState,
+            failure.message,
+          ),
+        ),
+      },
+      (homeObject) {
+        // right -> data (success)
+        // content
+        inputState.add(
+          ContentState(),
+        );
+        inputBanners.add(homeObject.homedata.banners);
+        inputServices.add(homeObject.homedata.services);
+        inputStores.add(homeObject.homedata.stores);
+      },
+    );
+  }
 
   @override
   void dispose() {
